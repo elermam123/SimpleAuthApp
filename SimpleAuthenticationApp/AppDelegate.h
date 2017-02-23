a9c1624b0b65e879784ace8e3b6752a75b6cb18c
Elerman <stafedapple2015@ya.ru>
2017-02-23 15:26:33 +0300

    Initial Commit

---
+++ SimpleAuthenticationApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleAuthenticationApp
+//
+//  Created by Elerman on 23.02.17.
+//  Copyright © 2017 Elerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
